3ef680e26c857b0ea78bdfb1bac17f16e46b1f1a
zashdnet <jsl305@protonmail.com>
2025-04-17 16:23:34 -0400

    Add Header component and update page.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+// filepath: /Users/javiersalido/harlow/components/Header.tsx
+export default function Header() {
+  return (
+    <header className="w-full p-4 bg-gray-800 text-white text-center">
+      <h1>Welcome to My App</h1>
+    </header>
+  );
+}
