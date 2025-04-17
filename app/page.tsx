8a61030e1181b33754e39e75db38bc019764ab97
zashdnet <jsl305@protonmail.com>
2025-04-17 16:06:04 -0400

    removing header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import header from '../components/header';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header /> {/* Inserted header component */}
+    
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
--- components/Header.tsx
+++
@@ -1,8 +0,0 @@
-// filepath: /Users/javiersalido/harlow/components/Header.tsx
-export default function header() {
-    return (
-      <header>
-        <h1>Harlow Header</h1>
-      </header>
-    );
-  }
