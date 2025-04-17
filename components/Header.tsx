a67c3a1595f8f5c706de98f00d6fe27d5b6df958
zashdnet <jsl305@protonmail.com>
2025-04-17 16:38:10 -0400

    Super App

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
-// filepath: /Users/javiersalido/harlow/components/Header.tsx
 export default function Header() {
   return (
     <header className="w-full p-4 bg-gray-800 text-white text-center">
-      <h1>Welcome to My App</h1>
+      <h1>Welcome to My Super App</h1>
     </header>
   );
 }
