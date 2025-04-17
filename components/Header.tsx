92e1e780e2d2cb12131177c1164b6203ef39d9ff
zashdnet <jsl305@protonmail.com>
2025-04-17 16:44:35 -0400

    Super Duper

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="w-full p-4 bg-gray-800 text-white text-center">
-      <h1>Welcome to My Super App</h1>
+      <h1>Welcome to My Super Duper App</h1>
     </header>
   );
 }
